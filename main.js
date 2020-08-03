b8ea809da609e86b31f11a271ed084e7e00aa9e9
franfiori29 <franfiori29@gmail.com>
2020-08-02 21:52:28 -0300

    Proyectos en mobile

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var menuDesplegable = function () {
-    let navLinks = document.querySelector(".nav-links");
-    let menuEstado = window.getComputedStyle(navLinks).getPropertyValue("display");
-    if (menuEstado == "none") {
-        navLinks.style.display = "block";
-    } else {
-        navLinks.style.display = "none"
-    }
-}
+  let navLinks = document.querySelector(".nav-links");
+  let menuEstado = window
+    .getComputedStyle(navLinks)
+    .getPropertyValue("display");
+  if (menuEstado == "none") {
+    navLinks.style.display = "block";
+  } else {
+    navLinks.style.display = "none";
+  }
+};
