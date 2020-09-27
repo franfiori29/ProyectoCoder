783ef4d4c351cb66065c62264a54945b1037e31e
francofiori29 <franfiori29@gmail.com>
2020-09-27 13:49:19 -0300

    commit final

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var menuDesplegable = function () {
+  let navLinks = document.querySelector(".nav-links");
+  let menuEstado = window
+    .getComputedStyle(navLinks)
+    .getPropertyValue("display");
+  if (menuEstado == "none") {
+    navLinks.style.display = "block";
+  } else {
+    navLinks.style.display = "none";
+  }
+};
